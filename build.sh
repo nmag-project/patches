df566829cda7b0b65e32adb34713da306eff07d2
Madison Hanberry <mhanberry1@gmail.com>
2018-08-28 12:53:49 -0400

    Added build.sh and fixed issues in Install.txt

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define the nmag version
+version="0.2.1";
+
+# Install Dependencies
+sudo apt install python ipython python-pip libreadline-dev g++ libblas-dev libreadline-dev make m4 gawk zlib1g-dev liblapack-dev mpich mpi-default-bin ocaml python-tables
+sudo pip install numpy pyvtk ipython py ply scipy
+
+# Untar archive
+tar xzvf nmag-$version.tar.gz
+
+# Navigate to the nmag directory
+cd nmag-$version
+
+# Replace the original Makefile with the modified Makefile
+mv Makefile Makefile.old
+cp ../Makefile .
+
+# Replace the original shell.py with the modifies shell.py
+mv nsim/interface/nsim/shell.py nsim/interface/nsim/shell.py.old
+cp ../shell.py nsim/interface/nsim
+
+# Convert old pytables camel casing to underscore notation
+grep -rl nsim -e isPyTablesFile | xargs sed -i s/isHDF5File/is_pytables_file/g
+grep -rl nsim -e isHDF5File | xargs sed -i s/isHDF5File/is_hdf5_file/g
+grep -rl nsim -e openFile | xargs sed -i s/openFile/open_file/g
+grep -rl nsim -e createGroup | xargs sed -i s/createGroup/create_group/g
+grep -rl nsim -e createArray | xargs sed -i s/createArray/create_array/g
+grep -rl nsim -e createCArray | xargs sed -i s/createCArray/create_carray/g
+grep -rl nsim -e createTable | xargs sed -i s/createTable/create_table/g
+grep -rl nsim -e getNode | xargs sed -i s/getNode/get_node/g
+
+# Compile the code
+if make; then
+	echo -e "\n\033[5mCompilation Complete.\033[0m The binaries can be foud in the nmag-$version/bin directory."
+else
+	echo -e "\n\033[5mCompilation Failed.\033[0m"
+fi
